66206c679162f81a7cc4e768e827d7a6b3e8ed9f
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-05-22 23:01:27 +0300

    simple commit

---
+++ app/src/main/java/com/example/ehasibu/purchases/Purchase_Order.kt
@@ -0,0 +1,31 @@
+package com.example.ehasibu.purchases
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ehasibu.R
+
+class Purchase_Order : Fragment() {
+
+    companion object {
+        fun newInstance() = Purchase_Order()
+    }
+
+    private val viewModel: PurchaseOrderViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_purchase_order, container, false)
+    }
+}
