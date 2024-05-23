ebaf077649de0313dc0de8e2236bbc87046d4cbb
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-05-23 07:59:15 +0300

    simple

--- app/src/main/java/com/example/ehasibu/purchases/Purchase_Order.kt
+++ app/src/main/java/com/example/ehasibu/purchases/Purchase_Order.kt
@@ -14,6 +14,7 @@ class Purchase_Order : Fragment() {
         fun newInstance() = Purchase_Order()
     }
 
+
     private val viewModel: PurchaseOrderViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
