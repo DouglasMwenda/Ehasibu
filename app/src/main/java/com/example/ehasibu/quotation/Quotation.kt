2b523dc388a3dbe6b09d69bb30d820ab12d0d445
Dorcas <dorcasjemosop@gmail.com>
2024-05-23 12:20:11 +0300

    sale

--- app/src/main/java/com/example/ehasibu/quotation/Quotation.kt
+++ app/src/main/java/com/example/ehasibu/quotation/Quotation.kt
@@ -1,4 +1,4 @@
-package com.example.ehasibu.sales
+package com.example.ehasibu.quotation
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
@@ -8,13 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.ehasibu.R
 
-class Sales : Fragment() {
+class Quotation : Fragment() {
 
     companion object {
-        fun newInstance() = Sales()
+        fun newInstance() = Quotation()
     }
 
-    private val viewModel: SalesViewModel by viewModels()
+    private val viewModel: QuotationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +26,6 @@ class Sales : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_sales, container, false)
+        return inflater.inflate(R.layout.fragment_quotation, container, false)
     }
 }
---
+++ app/src/main/java/com/example/ehasibu/quotation/QuotationViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ehasibu.quotation
+
+import androidx.lifecycle.ViewModel
+
+class QuotationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
