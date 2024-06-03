e34ed5a8e27d7c8940fc37649978be103c779b70
Dorcas <dorcasjemosop@gmail.com>
2024-06-03 08:28:23 +0300

    simple commit

--- app/src/main/java/com/example/ehasibu/dashboard/Dashboard.kt
+++ app/src/main/java/com/example/ehasibu/dashboard/Dashboard.kt
@@ -1,16 +1,13 @@
 package com.example.ehasibu.dashboard
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.ehasibu.R
 import com.example.ehasibu.databinding.FragmentDashboardBinding
 import com.google.android.material.navigation.NavigationView
@@ -25,156 +22,94 @@ class Dashboard : Fragment() {
         fun newInstance() = Dashboard()
     }
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        this.binding = binding
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
         return binding.root
-
-
-
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
 
         drawerLayout = binding.drawerLayout
         navView = binding.navView
         val toolbar = binding.toolbar
 
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
 
-        toggle = ActionBarDrawerToggle(requireActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-
+        toggle = ActionBarDrawerToggle(
+            requireActivity(),
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-
         setupNavigationView(navView)
     }
 
-
-        private fun setupNavigationView(navView: NavigationView) {
-            navView.setNavigationItemSelectedListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.nav_home -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_dashboard -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-
-                    R.id.sales -> {
-                        handleSubMenu(menuItem)
-                        true
-                    }
-                    R.id.purchases -> {
-                        handleSubMenu(menuItem)
-                        true
-                    }
-                    R.id.product_sales -> {
-                        Toast.makeText(context, "Product Sales clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.service_sales -> {
-                        Toast.makeText(context, "Service Sales clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.quotation -> {
-                        Toast.makeText(context, "Quotation clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.purchase_orders -> {
-                        Toast.makeText(context, "Purchase Orders clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.purchasebill -> {
-                        Toast.makeText(context, "Purchase Bill clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.vendor -> {
-                        Toast.makeText(context, "Vendor clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.customerinformation -> {
-                        Toast.makeText(context, "Customer Information clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_products -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_services -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_accounts -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_budget -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_expenses -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_reports -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_returns -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    R.id.nav_settings -> {
-                        Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
-                        true
-                    }
-                    else -> false
-
+    private fun setupNavigationView(navView: NavigationView) {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            Log.d("Menudebug", "Menu item ID: ${menuItem.itemId}")
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(context, "Home clicked", Toast.LENGTH_SHORT).show()
+                    true
                 }
+                R.id.nav_dashboard -> {
+                    Toast.makeText(context, "Dashboard clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_sales -> {
+
+                    true
+                }
+                R.id.nav_purchases -> {
+                    Toast.makeText(context, "Purchases clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_products -> {
+                    Toast.makeText(context, "Products clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_services -> {
+                    Toast.makeText(context, "Services clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_accounts -> {
+                    Toast.makeText(context, "Accounts clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_budget -> {
+                    Toast.makeText(context, "Budget clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_expenses -> {
+                    Toast.makeText(context, "Expenses clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_reports -> {
+                    Toast.makeText(context, "Reports clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_returns -> {
+                    Toast.makeText(context, "Returns clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_settings -> {
+                    Toast.makeText(context, "Settings clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
             }
         }
-
-        private fun handleSubMenu(menuItem: MenuItem) {
-            if (menuItem.isChecked) {
-                menuItem.isChecked = false
-                collapseSubMenu(menuItem)
-            } else {
-                menuItem.isChecked = true
-                expandSubMenu(menuItem)
-            }
-        }
-
-        private fun expandSubMenu(menuItem: MenuItem) {
-            for (i in 0 until menuItem.subMenu!!.size()) {
-                menuItem.subMenu!!.getItem(i).isVisible = true
-                menuItem.subMenu!!.getItem(i).isEnabled = true
-            }
-        }
-
-        private fun collapseSubMenu(menuItem: MenuItem) {
-            for (i in 0 until menuItem.subMenu!!.size()) {
-                menuItem.subMenu!!.getItem(i).isVisible = false
-                menuItem.subMenu!!.getItem(i).isEnabled = false
-            }
-        }
-            }
+    }
 
 
-
-
+}
---
+++ app/src/main/java/com/example/ehasibu/productsales/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ehasibu.productsales
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
