6ed9f013a97129681006de5de76706b4315d6788
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-06-03 22:20:53 +0300

    simple

--- app/src/main/java/com/example/ehasibu/accounts/AccountsFragment.kt
+++ app/src/main/java/com/example/ehasibu/accounts/AccountsFragment.kt
@@ -16,11 +16,6 @@ class AccountsFragment : Fragment() {
 
     private val viewModel: AccountsViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
