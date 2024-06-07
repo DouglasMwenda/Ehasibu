11ac1ec2035577ef1c94ca5890173acd589cdb35
Dorcas <dorcasjemosop@gmail.com>
2024-06-07 14:33:07 +0300

    simple commit

--- app/src/main/java/com/ehasibu/home/HomeFragment.kt
+++ app/src/main/java/com/ehasibu/home/HomeFragment.kt
@@ -10,7 +10,11 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.example.ehasibu.R
 import com.example.ehasibu.databinding.FragmentHomeBinding
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -18,6 +22,7 @@ import com.github.mikephil.charting.data.PieEntry
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var pieChart: PieChart
+    private lateinit var barChart: BarChart
     private val viewModel: HomeViewModel by viewModels()
 
 
@@ -59,6 +64,34 @@ class HomeFragment : Fragment() {
         pieChart.description.text= "Sales Per Item"
         pieChart.centerText = "List"
         pieChart.animateY(2000)
+
+
+        barChart = binding.BarChart
+
+        val barList: ArrayList<BarEntry> = ArrayList()
+        barList.add(BarEntry(100f,100f))
+        barList.add(BarEntry(101f,101f))
+        barList.add(BarEntry(102f,102f))
+        barList.add(BarEntry(103f,103f))
+        barList.add(BarEntry(104f,104f))
+
+        val barDataSet= BarDataSet(barList,"List")
+
+        val barChartcolors = mutableListOf<Int>()
+        colors.add(ContextCompat.getColor(requireContext(), R.color.colorBlue))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.colorRed))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.colorYellow))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.colorGreen))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.colorMaroon))
+
+        barDataSet.colors= barChartcolors
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 15f
+        val barData = BarData(barDataSet)
+        barChart.data = barData
+        barChart.setFitBars(true)
+        barChart.description.text= "Bar Chart"
+        barChart.animateY(2000)
 
         return  binding.root
     }
