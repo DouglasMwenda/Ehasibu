6b14273298161e13ec3bcbc4523c674d6bca60d6
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-08-12 14:34:17 +0300

    simple

--- app/src/main/java/com/example/ehasibu/purchaseorder/data/PoRequest.kt
+++ app/src/main/java/com/example/ehasibu/purchaseorder/data/PoRequest.kt
@@ -3,5 +3,5 @@ package com.example.ehasibu.purchaseorder.data
 data class PoRequest(
     val deliveryDate: String,
     val products: List<ProductX>,
-    val vendorId: String
+    val vendorId: String,
 )
