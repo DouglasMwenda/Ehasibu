d347309ff3b41795269244ea55aca133e353a928
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-08-27 17:47:11 +0300

    simple

--- app/src/main/java/com/example/ehasibu/utils/api/APIConsumer.kt
+++ app/src/main/java/com/example/ehasibu/utils/api/APIConsumer.kt
@@ -3,7 +3,6 @@ package com.example.ehasibu.utils.api
 import com.example.ehasibu.customerinformation.data.CustomerRequest
 import com.example.ehasibu.customerinformation.data.CustomerResItem
 import com.example.ehasibu.customerinformation.data.CustomerResponse
-import com.example.ehasibu.customerinformation.data.UpdateCustomerRequest
 import com.example.ehasibu.login.ApiResponse
 import com.example.ehasibu.login.data.AuthUserResponse
 import com.example.ehasibu.login.data.OtpRequest
@@ -26,6 +25,10 @@ import com.example.ehasibu.purchaseorder.data.OrderEntity
 import com.example.ehasibu.purchaseorder.data.OrderResponse
 import com.example.ehasibu.purchaseorder.data.PoRequest
 import com.example.ehasibu.purchaseorder.data.PoResponse
+import com.example.ehasibu.vendors.moddel.AddRequest
+import com.example.ehasibu.vendors.moddel.AddVendorResponse
+import com.example.ehasibu.vendors.moddel.EditResponse
+import com.example.ehasibu.vendors.moddel.EditVRequest
 import com.example.ehasibu.vendors.moddel.VendorResponse
 import retrofit2.Call
 import retrofit2.Response
@@ -83,14 +86,6 @@ interface APIConsumer {
     suspend fun getCustomers(): Response<ApiResponse<List<CustomerResItem>>>
 
 
-    @PUT("customers/customers/{id}")
-    suspend fun updateCustomer(@Body customer: UpdateCustomerRequest): Response<ApiResponse<CustomerResItem>>
-
-    @DELETE("customers/{id}")
-    suspend fun deleteCustomer(@Query("id") id: Int): Response<DelResponse>
-
-
-
     //Purchases
     @GET("purchases/getAllPurchases")
     suspend fun fetchOrders(): Response<OrderResponse<List<OrderEntity>>>
@@ -99,14 +94,19 @@ interface APIConsumer {
     suspend fun approveOrder(@Query("id") id: String): Response<ApproveResponse>
 
     @POST("purchases/addPurchase")
-
     suspend fun addOrder(@Body order: PoRequest): Response<ApiResponse<PoResponse>>
 
 
-
-    @POST("vendors/fetchAll")
+    @GET("vendors/fetchAll")
     suspend fun fetchVendors(): Response<VendorResponse>
 
+    @PUT("vendors/vendorId")
+    suspend fun updateVendor(@Body editRequest: EditVRequest): Response<EditResponse>
+    @DELETE("vendors/vendorId")
+    suspend fun deleteVendor(@Query("vendorId") vendorId: String): Response<DelResponse>
+
+    @POST("vendors/add")
+    suspend fun addVendor(@Body vendor: AddRequest): Response<AddVendorResponse>
 }
 
 
