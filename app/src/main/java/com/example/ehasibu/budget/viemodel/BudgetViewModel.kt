7ba9f7857ab2bbb9f807eb4720202203d94d617b
Dorcas <dorcasjemosop@gmail.com>
2024-09-04 16:32:56 +0300

    changes

--- app/src/main/java/com/example/ehasibu/budget/viemodel/BudgetViewModel.kt
+++ app/src/main/java/com/example/ehasibu/budget/viemodel/BudgetViewModel.kt
@@ -2,6 +2,7 @@ package com.example.ehasibu.budget.viemodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.ehasibu.budget.data.Entity
 import com.example.ehasibu.budget.repo.BudgetRepository
@@ -10,8 +11,8 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class BudgetViewModel (private val repo : BudgetRepository): ViewModel() {
-    private val _budgets = MutableLiveData<List<Entity>?> (emptyList())
-    val budgets : MutableLiveData<List<Entity>?> get() = _budgets
+    private val _budgets = MutableLiveData<List<Entity>?>(emptyList())
+    val budgets: MutableLiveData<List<Entity>?> get() = _budgets
 
     init {
         getBudgets()
@@ -19,20 +20,29 @@ class BudgetViewModel (private val repo : BudgetRepository): ViewModel() {
 
     private fun getBudgets() {
         viewModelScope.launch {
-            while(isActive)
+            while (isActive)
                 try {
                     val response = repo.getAllBudgets()
-                    if(response.isSuccessful) {
+                    if (response.isSuccessful) {
                         response.body()?.let {
                             _budgets.value = it
                         }
                     }
                     delay(10000)
-                }
-                catch (t: Throwable) {
+                } catch (t: Throwable) {
 
                 }
         }
     }
 
+    class BudgetProvider(val repo: BudgetRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(BudgetViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return BudgetViewModel(repo) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+
+    }
 }
